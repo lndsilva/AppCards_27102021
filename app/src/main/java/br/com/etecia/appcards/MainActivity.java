60d122402f4e91044eb734481367e11ee0d8608c
lndsilva <lndsilva@hotmail.com>
2021-10-27 20:25:42 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/appcards/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
